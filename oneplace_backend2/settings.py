9a53b6ed07fc834de8dbd7fd92e71c0b062b0b4d
vigneshdonthi <vigneshdonthi1@gmail.com>
2025-04-25 13:33:48 +0530

    Update settings.py according to render

--- oneplace_backend2/settings.py
+++ oneplace_backend2/settings.py
@@ -16,24 +16,44 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+
+
+
+
+
+
+
+
+
+
+RENDER_DOMAIN = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+
+if RENDER_DOMAIN:
+    CSRF_TRUSTED_ORIGINS = [f'https://{RENDER_DOMAIN}']
+    ALLOWED_HOSTS = [RENDER_DOMAIN,"*"]
+else:
+    CSRF_TRUSTED_ORIGINS = ['http://localhost:8000']
+    ALLOWED_HOSTS = ['localhost', '127.0.0.1',"*"]
+
+
+
+
+
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dd=p(py%&!jt39!+_bvd+fvdti7@+d1ah3bnl@6^va9bz9r8@p'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
 
-ALLOWED_HOSTS = [
-    'oneplaceapp-l8vy.onrender.com',
-    'localhost',
-    '127.0.0.1',
-    "*",
-]
 
-CSRF_TRUSTED_ORIGINS = [
-    'https://oneplaceapp-l8vy.onrender.com',]
+
+
+
+
 
 # Application definition
 
