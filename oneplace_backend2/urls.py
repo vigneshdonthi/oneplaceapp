f9a902f390b2034d57fea338d794e34235586881
vigneshdonthi <vigneshdonthi1@gmail.com>
2025-05-06 23:03:32 +0530

    Update urls.py added sample

--- oneplace_backend2/urls.py
+++ oneplace_backend2/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from core.views import CustomLoginView,RegisterView,UserProfileView,LogoutView
+from core.views import CustomLoginView,RegisterView,UserProfileView,LogoutView,SampleJSONView
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -32,6 +32,7 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/logout/', LogoutView.as_view(), name='api-logout'),
+    path('api/sample/', SampleJSONView.as_view(), name='sample-json'),
 ]
    
 
